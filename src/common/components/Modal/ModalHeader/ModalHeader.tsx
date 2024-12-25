70cdc7e004360bea8df55e540adb86fe223d246d
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-12-26 04:18:10 +0800

    refactor: split modal header into title and description

--- src/common/components/Modal/ModalHeader/ModalHeader.tsx
+++ src/common/components/Modal/ModalHeader/ModalHeader.tsx
@@ -1,33 +1,10 @@
-import {
-  Title,
-  Description,
-  type DialogDescriptionProps,
-} from "@radix-ui/react-dialog";
-import { type ComponentPropsWithoutRef, type PropsWithChildren } from "react";
-
-import {
-  Heading,
-  type HeadingProps,
-} from "@/common/components/Heading/Heading";
-import { cn } from "@/common/functions";
 import { modalTheme } from "../Modal.theme";
 
-export interface ModalHeaderProps extends ComponentPropsWithoutRef<"div"> {
-  className?: string;
-  description?: string | React.ReactNode;
-  descriptionProps?: DialogDescriptionProps;
-  headingProps?: HeadingProps<"h2">;
-}
-
 export const ModalHeader = ({
-  children,
-  description,
   className,
-  descriptionProps,
-  headingProps,
   ...props
-}: PropsWithChildren<ModalHeaderProps>) => {
-  const { header, headerDescription } = modalTheme({
+}: React.HTMLAttributes<HTMLDivElement>) => {
+  const { header } = modalTheme({
     size: { initial: "sm", md: "md" },
   });
 
@@ -37,20 +14,6 @@ export const ModalHeader = ({
         className,
       })}
       {...props}
-    >
-      <Title asChild className={cn(headingProps?.className)} {...headingProps}>
-        <Heading as="h2">{children}</Heading>
-      </Title>
-      {description && (
-        <Description
-          className={headerDescription({
-            className: descriptionProps?.className,
-          })}
-          {...descriptionProps}
-        >
-          {description}
-        </Description>
-      )}
-    </div>
+    />
   );
 };
