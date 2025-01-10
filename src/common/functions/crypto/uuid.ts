8c96da82ec02c5cdddef48040f8ac5feae9efb3e
David Lin <13061926+DavidLHW@users.noreply.github.com>
2025-01-11 05:25:28 +0800

    chore: uuid function to not use rotating salt by default

--- src/common/functions/crypto/uuid.ts
+++ src/common/functions/crypto/uuid.ts
@@ -1,9 +1,8 @@
 import { v4, v5 } from "uuid";
 import { hash } from "./hash";
-import { rotatingSaltStartOfMonth } from "./rotatingSaltStartOfMonth";
 
 export function uuid(...args: string[]) {
   if (!args.length) return v4();
 
-  return v5(hash(...args, rotatingSaltStartOfMonth()), v5.DNS);
+  return v5(hash(...args), v5.DNS);
 }
