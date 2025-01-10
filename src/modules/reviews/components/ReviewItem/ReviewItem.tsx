70711f9bdfac4a601bc29207c2b5d5c30146ae29
David Lin <13061926+DavidLHW@users.noreply.github.com>
2025-01-11 05:29:04 +0800

    feat(ui): implement review item view event

--- src/modules/reviews/components/ReviewItem/ReviewItem.tsx
+++ src/modules/reviews/components/ReviewItem/ReviewItem.tsx
@@ -1,4 +1,5 @@
 import { useCallback } from "react";
+import { useSession } from "next-auth/react";
 
 import { LockCtaOverlay } from "@/common/components/LockCtaOverlay";
 import { type Review } from "@/modules/reviews/types";
@@ -8,6 +9,7 @@ import { ReviewerGroup } from "./ReviewerGroup";
 import { RevieweeGroup } from "./RevieweeGroup";
 import { ReviewBody } from "./ReviewBody";
 import { ReviewModal } from "./ReviewModal";
+import { ReviewItemViewEventTracker } from "../ReviewItemViewEventTracker";
 
 export type ReviewItemProps = ReviewItemVariants & {
   review: Review;
@@ -24,6 +26,7 @@ export const ReviewItem = ({
   isMocked = false,
   seeMore,
 }: ReviewItemProps) => {
+  const session = useSession();
   const { wrapper, headingContainer, body } = reviewItemTheme({
     size: { initial: "sm", md: "md" },
   });
@@ -38,29 +41,29 @@ export const ReviewItem = ({
     [review, isMocked, variant],
   );
 
-  return (
-    <>
-      {isLocked ? (
-        <div
-          className={wrapper({
-            className: "w-full max-w-full hover:bg-inherit lg:max-w-prose",
-          })}
-          data-variant="full-width"
-          data-test="review"
-        >
-          <ReviewHeader />
-          <div className={body({ isLocked })}>
-            <LockCtaOverlay size="sm" ctaType="review" variant="border" />
-          </div>
-        </div>
-      ) : (
-        <ReviewModal review={review} variant={variant} seeMore={seeMore}>
-          <div className={wrapper()} data-test="review">
-            <ReviewHeader />
-            <ReviewBody review={review} />
-          </div>
-        </ReviewModal>
-      )}
-    </>
+  return !(session.status === "authenticated") || isLocked ? (
+    <div
+      className={wrapper({
+        className: "w-full max-w-full hover:bg-inherit lg:max-w-prose",
+      })}
+      data-variant="full-width"
+      data-test="review"
+    >
+      <ReviewHeader />
+      <div className={body({ isLocked })}>
+        <LockCtaOverlay size="sm" ctaType="review" variant="border" />
+      </div>
+    </div>
+  ) : (
+    <ReviewModal review={review} variant={variant} seeMore={seeMore}>
+      <div className={wrapper()} data-test="review">
+        <ReviewHeader />
+        <ReviewBody review={review} />
+        <ReviewItemViewEventTracker
+          reviewId={review.id}
+          triggeringUserId={session.data.user.id}
+        />
+      </div>
+    </ReviewModal>
   );
 };
