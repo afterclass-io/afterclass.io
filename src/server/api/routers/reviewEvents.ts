93fbf212f07e848286d652b6521e2ae96549294c
David Lin <13061926+DavidLHW@users.noreply.github.com>
2025-01-11 05:28:19 +0800

    feat(router): add review event protected mutation procedure

---
+++ src/server/api/routers/reviewEvents.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
+import { ReviewEventType } from "@prisma/client";
+import { rotatingSaltStartOfHour, uuid } from "@/common/functions/crypto";
+
+export const reviewEventsRouter = createTRPCRouter({
+  track: protectedProcedure
+    .input(
+      z.object({
+        reviewId: z.string(),
+        eventType: z.nativeEnum(ReviewEventType),
+        triggeringUserId: z.string(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const eventId = uuid(
+        input.reviewId,
+        input.eventType,
+        input.triggeringUserId,
+        rotatingSaltStartOfHour(),
+      );
+      return await ctx.db.reviewEvents.upsert({
+        where: { id: eventId },
+        update: {},
+        create: {
+          id: eventId,
+          eventType: input.eventType,
+          reviewId: input.reviewId,
+          triggeringUserId: input.triggeringUserId,
+        },
+      });
+    }),
+});
