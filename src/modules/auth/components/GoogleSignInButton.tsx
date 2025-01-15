4ed26424f54673d69bd04310073650b79e06da00
David Lin <13061926+DavidLHW@users.noreply.github.com>
2025-01-16 03:00:20 +0800

    refactor: google oauth btn loading prop

--- src/modules/auth/components/GoogleSignInButton.tsx
+++ src/modules/auth/components/GoogleSignInButton.tsx
@@ -57,22 +57,7 @@ export const GoogleSignInButton = ({
       onClick={handleGoogleSignIn}
       tabIndex={4}
       disabled={isLoading}
-      iconLeft={
-        isLoading ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="mr-2 h-4 w-4 animate-spin"
-          >
-            <path d="M21 12a9 9 0 1 1-6.219-8.56" />
-          </svg>
-        ) : undefined
-      }
+      loading={isLoading}
     >
       {children}
     </Button>
