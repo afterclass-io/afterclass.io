598b9274f0e8d39530b56955e1ebcc32da06a99e
David Lin <13061926+DavidLHW@users.noreply.github.com>
2025-01-27 15:00:03 +0800

    fix: vote count scale and color

--- src/modules/reviews/components/ReviewItem/ReviewVoteGroup/ReviewVoteGroup.tsx
+++ src/modules/reviews/components/ReviewItem/ReviewVoteGroup/ReviewVoteGroup.tsx
@@ -1,11 +1,8 @@
 import { Button } from "@/common/components/Button";
 import { ArrowUpIcon } from "@/common/components/CustomIcon";
+import { formatNumberShortScale } from "@/common/functions";
 
-export const ReviewVoteGroup = ({
-  likeCount,
-}: {
-  likeCount: string | number;
-}) => {
+export const ReviewVoteGroup = ({ likeCount }: { likeCount: number }) => {
   return (
     <div className="flex h-8 items-center gap-1 rounded-full border border-border-default bg-element-tertiary">
       <Button
@@ -16,7 +13,9 @@ export const ReviewVoteGroup = ({
         iconLeft={<ArrowUpIcon className="h-4 w-4" />}
         rounded
       />
-      <span className="text-text-on-low text-xs">{likeCount}</span>
+      <span className="text-xs text-text-em-mid">
+        {formatNumberShortScale(likeCount)}
+      </span>
       <Button
         variant="tertiary"
         size="sm"
