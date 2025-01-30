285a83abee17effa8f545629db2013bc67f1b2e8
David Lin <13061926+DavidLHW@users.noreply.github.com>
2025-01-30 13:12:08 +0800

    fix: prisma telemetry integration with sentry

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -3,6 +3,7 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import { PrismaInstrumentation } from "@prisma/instrumentation";
 
 Sentry.init({
   dsn: "https://81c51704b5a973abc295473c5b430131@o4508338523537408.ingest.us.sentry.io/4508338554208256",
@@ -13,6 +14,10 @@ Sentry.init({
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
-  integrations: [Sentry.prismaIntegration()],
+  integrations: [
+    Sentry.prismaIntegration({
+      prismaInstrumentation: new PrismaInstrumentation(),
+    }),
+  ],
   ignoreErrors: [/^UNAUTHORIZED$/, /AbortError/i],
 });
