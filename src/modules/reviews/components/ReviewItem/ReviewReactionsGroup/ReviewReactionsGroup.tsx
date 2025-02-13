ef1e7c28dcfe3f84710577975207b4dc6ec3c422
David Lin <13061926+DavidLHW@users.noreply.github.com>
2025-02-14 02:20:22 +0800

    feat(ui): reaction group interactivity

--- src/modules/reviews/components/ReviewItem/ReviewReactionsGroup/ReviewReactionsGroup.tsx
+++ src/modules/reviews/components/ReviewItem/ReviewReactionsGroup/ReviewReactionsGroup.tsx
@@ -1,65 +1,82 @@
 "use client";
 import { useSession } from "next-auth/react";
+import { ReviewReactionType as DbReviewReactionType } from "@prisma/client";
 
-import { Button } from "@/common/components/Button";
 import { api } from "@/common/tools/trpc/react";
 import { ReviewReactionType } from "@/modules/reviews/types";
+import { Tag } from "@/common/components/Tag";
 
 export const ReviewReactionsGroup = ({ reviewId }: { reviewId: string }) => {
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    e.stopPropagation();
-  };
-
   const { data: session } = useSession();
 
   const reviewReactionsQuery = api.reviewReactions.getByReviewId.useQuery({
     reviewId,
+  });
+  const utils = api.useUtils();
+  const { mutate: upsertReaction } = api.reviewReactions.upsert.useMutation({
+    onSuccess: () => {
+      utils.reviewReactions.getByReviewId.refetch({ reviewId });
+    },
   });
 
   if (!reviewReactionsQuery.data || !session) {
     return;
   }
 
-  const groupedReactions = reviewReactionsQuery.data.reduce(
+  const reactionsMetadataMap = reviewReactionsQuery.data.reduce(
     (acc, reviewReaction) => {
       const reaction = reviewReaction.reaction;
 
       if (!acc[reaction]) {
         acc[reaction] = {
           count: 0,
-          hasThisUserReacted: reviewReaction.reactingUserId === session.user.id,
+          hasThisUserReacted: false,
         };
       }
 
       acc[reaction].count += 1;
+      acc[reaction].hasThisUserReacted =
+        acc[reaction].hasThisUserReacted ||
+        reviewReaction.reactingUserId === session.user.id;
 
       return acc;
     },
     {} as Record<string, { count: number; hasThisUserReacted: boolean }>,
   );
 
+  const handleReactionChange = (newReaction: DbReviewReactionType) => {
+    if (reactionsMetadataMap[newReaction]!.hasThisUserReacted) {
+      upsertReaction({
+        reviewId,
+      });
+    } else {
+      upsertReaction({
+        reviewId,
+        reaction: newReaction,
+      });
+    }
+  };
+
   return (
-    <div className="flex gap-1">
-      {Object.entries(groupedReactions).map(
+    <div className="flex gap-2">
+      {Object.entries(reactionsMetadataMap).map(
         ([reaction, { count, hasThisUserReacted }]) => (
-          <Button
-            size="sm"
-            variant={hasThisUserReacted ? "secondary" : "tertiary"}
-            rounded
-            iconLeft={
-              <span className="mx-[-2px] flex h-full w-fit items-center text-base">
-                {
-                  ReviewReactionType[
-                    reaction as keyof typeof ReviewReactionType
-                  ]
-                }
-              </span>
-            }
-            onClick={handleClick}
+          <Tag
+            onClick={(e) => {
+              e.stopPropagation();
+              handleReactionChange(reaction as DbReviewReactionType);
+            }}
+            key={reaction}
+            clickable
+            active={hasThisUserReacted}
+            className="flex min-w-fit gap-1 px-2 py-0"
+            asChild
           >
-            {count}
-          </Button>
+            <span className="text-lg">
+              {ReviewReactionType[reaction as keyof typeof ReviewReactionType]}
+            </span>
+            <span>{count}</span>
+          </Tag>
         ),
       )}
     </div>
