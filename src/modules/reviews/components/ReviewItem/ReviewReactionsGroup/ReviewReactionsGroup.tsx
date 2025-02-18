bd99f7124ff03130f0c3da29417c85cd2abc3750
David Lin <13061926+DavidLHW@users.noreply.github.com>
2025-02-18 17:38:51 +0800

    feat(ui): optimistic updates on review reactions

--- src/modules/reviews/components/ReviewItem/ReviewReactionsGroup/ReviewReactionsGroup.tsx
+++ src/modules/reviews/components/ReviewItem/ReviewReactionsGroup/ReviewReactionsGroup.tsx
@@ -1,21 +1,83 @@
 "use client";
 import { useSession } from "next-auth/react";
-import { ReviewReactionType as DbReviewReactionType } from "@prisma/client";
+import {
+  ReviewReactionType as DbReviewReactionType,
+  ReviewEventType,
+} from "@prisma/client";
 
 import { api } from "@/common/tools/trpc/react";
-import { ReviewReactionType } from "@/modules/reviews/types";
 import { Tag } from "@/common/components/Tag";
+import { useEdgeConfigs } from "@/common/hooks";
+import { ReviewReactionType } from "@/modules/reviews/types";
 
 export const ReviewReactionsGroup = ({ reviewId }: { reviewId: string }) => {
   const { data: session } = useSession();
+  const ecfg = useEdgeConfigs();
 
   const reviewReactionsQuery = api.reviewReactions.getByReviewId.useQuery({
     reviewId,
   });
   const utils = api.useUtils();
+  const { mutate: track } = api.reviewEvents.track.useMutation();
   const { mutate: upsertReaction } = api.reviewReactions.upsert.useMutation({
-    onSuccess: async () => {
-      await utils.reviewReactions.getByReviewId.refetch({ reviewId });
+    onMutate: async ({ reviewId, reaction, userId }) => {
+      // Cancel any outgoing refetches
+      // (so they don't overwrite our optimistic update)
+      await utils.reviewReactions.getByReviewId.cancel();
+
+      // Snapshot the previous value
+      const previousReactions = utils.reviewReactions.getByReviewId.getData({
+        reviewId,
+      });
+
+      // Optimistically update to the new value
+      utils.reviewReactions.getByReviewId.setData(
+        { reviewId },
+        (oldQueryData) => {
+          const now = new Date();
+          const newReaction = {
+            reaction: reaction as DbReviewReactionType,
+            reactingUserId: userId ?? session?.user.id ?? "",
+            reviewId,
+            createdAt: now,
+            updatedAt: now,
+          };
+
+          if (!oldQueryData) return [newReaction];
+
+          // Remove the previous reaction if it exists
+          const updatedReactions = oldQueryData.filter(
+            (reaction) =>
+              reaction.reactingUserId !== newReaction.reactingUserId,
+          );
+
+          return [...updatedReactions, newReaction];
+        },
+      );
+
+      // Return a context object with the snapshotted value
+      return { previousReactions };
+    },
+    onError: (_err, _variables, context) => {
+      // Rollback to the previous value if mutation fails
+      utils.reviewReactions.getByReviewId.setData(
+        { reviewId },
+        context?.previousReactions,
+      );
+    },
+    onSuccess: (_, { userId }) => {
+      if (ecfg.enableReviewEventsTracking) {
+        const triggeringUserId = userId ?? session?.user.id;
+
+        track({
+          reviewId,
+          triggeringUserId,
+          eventType: ReviewEventType.REACTION,
+        });
+      }
+    },
+    onSettled: () => {
+      void utils.reviewReactions.getByReviewId.invalidate({ reviewId });
     },
   });
 
