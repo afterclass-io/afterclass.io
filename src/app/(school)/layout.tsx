8d39103a37c0dcb6799543d5fd78247383ced538
David Lin <13061926+DavidLHW@users.noreply.github.com>
2025-04-23 05:54:49 +0800

    style: reduce y-margins on main layout

--- src/app/(school)/layout.tsx
+++ src/app/(school)/layout.tsx
@@ -2,6 +2,6 @@ import { type PropsWithChildren } from "react";
 
 export default async function SchoolLayout({ children }: PropsWithChildren) {
   return (
-    <div className="mx-auto my-2 max-w-screen-lg p-4 md:my-8">{children}</div>
+    <div className="mx-auto my-2 max-w-screen-lg p-4 md:my-4">{children}</div>
   );
 }
