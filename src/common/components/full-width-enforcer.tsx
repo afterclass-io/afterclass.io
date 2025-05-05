f73aa7352d3042ef137b68ba01ef34ca8b8b6898
David Lin <13061926+DavidLHW@users.noreply.github.com>
2025-05-05 00:13:11 -0700

    style: fix mobile search result item warp

--- src/common/components/full-width-enforcer.tsx
+++ src/common/components/full-width-enforcer.tsx
@@ -1,7 +1,16 @@
-export const FullWidthEnforcer = () => {
+import { cn } from "@/common/functions";
+
+export const FullWidthEnforcer = ({
+  className,
+  ...props
+}: React.ComponentProps<"div">) => {
   return (
     // weird hack to make sure the review section width is consistent
-    <div aria-hidden className="h-0 max-w-prose text-transparent select-none">
+    <div
+      aria-hidden
+      className={cn("h-0 max-w-prose text-transparent select-none", className)}
+      {...props}
+    >
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
--- src/modules/search/components/SearchResult/SearchResultItem/SearchResultItem.tsx
+++ src/modules/search/components/SearchResult/SearchResultItem/SearchResultItem.tsx
@@ -26,47 +26,45 @@ export const SearchResultItem = ({
 }) => {
   const { data: session } = useSession();
   return (
-    <>
-      <FullWidthEnforcer />
-      <ProgressLink
-        href={href}
-        variant="outline"
-        className="bg-card flex h-fit w-full items-center justify-between gap-2 rounded-lg border p-3 whitespace-normal md:gap-4 md:p-4"
-        data-test="search-result"
-      >
-        <div className="flex flex-[1_0_0%] flex-col items-start justify-center gap-2 md:gap-4">
-          <div className="flex items-center gap-4 self-stretch">
-            <SchoolIcon
-              className="mt-[2px] size-4 flex-none md:size-6"
-              school={school}
-            />
+    <ProgressLink
+      href={href}
+      variant="outline"
+      className="bg-card flex h-fit w-full items-center justify-between gap-2 rounded-lg border p-3 whitespace-normal has-[>svg]:px-5 md:gap-4 md:p-4"
+      data-test="search-result"
+    >
+      <div className="flex flex-[1_0_0%] flex-col items-start justify-center space-y-2 md:space-y-4">
+        <FullWidthEnforcer className="mb-0" />
+        <div className="flex items-center gap-4 self-stretch">
+          <SchoolIcon
+            className="mt-[2px] size-4 flex-none md:size-6"
+            school={school}
+          />
+          <Heading
+            as="h1"
+            className="text-accent-foreground text-left tracking-tight md:text-lg"
+          >
+            {title}
+          </Heading>
+          {subtitle && (
             <Heading
-              as="h1"
-              className="text-accent-foreground text-left tracking-tight md:text-lg"
+              as="h2"
+              className="text-muted-foreground font-normal tracking-tight md:text-lg"
             >
-              {title}
+              {subtitle}
             </Heading>
-            {subtitle && (
-              <Heading
-                as="h2"
-                className="text-muted-foreground font-normal tracking-tight md:text-lg"
-              >
-                {subtitle}
-              </Heading>
-            )}
-          </div>
-          <div className="text-muted-foreground flex items-center gap-2 md:gap-4">
-            {session &&
-              filterStats?.map((stat, index) => (
-                <FilterItemStats key={index} {...stat} />
-              ))}
-          </div>
+          )}
         </div>
-        <ChevronRightIcon
-          size={24}
-          className="text-muted-foreground size-4 flex-none md:size-6"
-        />
-      </ProgressLink>
-    </>
+        <div className="text-muted-foreground flex items-center gap-2 md:gap-4">
+          {session &&
+            filterStats?.map((stat, index) => (
+              <FilterItemStats key={index} {...stat} />
+            ))}
+        </div>
+      </div>
+      <ChevronRightIcon
+        size={24}
+        className="text-muted-foreground size-4 flex-none md:size-6"
+      />
+    </ProgressLink>
   );
 };
