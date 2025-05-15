611409faf8667d5fdd217f6de1630e70e23ceb2d
David Lin <13061926+DavidLHW@users.noreply.github.com>
2025-05-15 12:35:03 -0400

    chore: disable hackathon voting

--- src/app/(school)/ui-hack-2025/_components/submission.tsx
+++ src/app/(school)/ui-hack-2025/_components/submission.tsx
@@ -1,6 +1,4 @@
 "use client";
-import Link from "next/link";
-
 import { buttonVariants } from "@/common/components/button";
 import { Heading } from "@/common/components/heading";
 import { cn } from "@/common/functions";
@@ -10,6 +8,7 @@ import { VoteButton } from "./vote-button";
 import { api } from "@/common/tools/trpc/react";
 import { useSession } from "next-auth/react";
 import { Separator } from "@/common/components/separator";
+import { toast } from "sonner";
 
 export const Submission = ({
   id,
@@ -26,71 +25,6 @@ export const Submission = ({
 
   const getVotesBySubmissionQuery = api.hackVotes.getBySubmission.useQuery({
     hackSubmissionId: id,
-  });
-
-  const utils = api.useUtils();
-  const { mutate: likeOrUnlike } = api.hackVotes.voteOrUnvote.useMutation({
-    onMutate: async ({ hackSubmissionId, weight }) => {
-      // Cancel any outgoing refetches
-      // (so they don't overwrite our optimistic update)
-      await utils.hackVotes.getBySubmission.cancel();
-
-      // Snapshot the previous value
-      const prev = utils.hackVotes.getBySubmission.getData({
-        hackSubmissionId,
-      });
-
-      // Optimistically update to the new value
-      utils.hackVotes.getBySubmission.setData(
-        { hackSubmissionId },
-        (oldQueryData) => {
-          if (!oldQueryData) return [];
-
-          const userId = session?.user.id;
-          if (!userId) return oldQueryData;
-
-          const userVoteIdx = oldQueryData.findIndex(
-            (vote) => vote.voterId === userId,
-          );
-          const userVote = oldQueryData.splice(userVoteIdx, 1)[0];
-
-          if (!userVote)
-            return [
-              ...oldQueryData,
-              {
-                voterId: userId,
-                weight,
-                hackSubmissionId,
-                createdAt: new Date(),
-                updatedAt: new Date(),
-              },
-            ];
-
-          return [...oldQueryData, { ...userVote, weight }];
-        },
-      );
-
-      // Return a context object with the snapshotted value
-      return { prev };
-    },
-    onError: (_err, _variables, context) => {
-      // Rollback to the previous value if mutation fails
-      utils.hackVotes.getBySubmission.setData(
-        { hackSubmissionId: id },
-        context?.prev,
-      );
-    },
-    // onSuccess: (_, { weight }) => {
-    //   if (ecfg.enableReviewEventsTracking) {
-    //     const eventType =
-    //       weight > 0 ? ReviewEventType.UPVOTE : ReviewEventType.DOWNVOTE;
-
-    //     track({ hackSubmissionId, eventType });
-    //   }
-    // },
-    onSettled: () => {
-      void utils.hackVotes.getBySubmission.invalidate({ hackSubmissionId: id });
-    },
   });
 
   const totalVotes = getVotesBySubmissionQuery.data?.reduce(
@@ -141,15 +75,13 @@ export const Submission = ({
         voted={userVote}
         totalVotes={totalVotes ?? 0}
         onVote={() => {
-          likeOrUnlike({
-            hackSubmissionId: id,
-            weight: 1,
+          toast.info("Voting Phase has concluded", {
+            description: "Thank you for your participation!",
           });
         }}
         onUnvote={() => {
-          likeOrUnlike({
-            hackSubmissionId: id,
-            weight: 0,
+          toast.info("Voting Phase has concluded", {
+            description: "Thank you for your participation!",
           });
         }}
       />
--- src/server/api/hackVotes/router.ts
+++ src/server/api/hackVotes/router.ts
@@ -1,9 +1,7 @@
 import { createTRPCRouter } from "@/server/api/trpc";
 
 import { getBySubmission } from "./getBySubmission";
-import { voteOrUnvote } from "./voteOrUnvote";
 
 export const hackVotesRouter = createTRPCRouter({
   getBySubmission,
-  voteOrUnvote,
 });
