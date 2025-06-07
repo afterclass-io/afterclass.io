6025480b3ae0b3b8320c2a30bc8daac396dae73e
onebignick <nicholas.zhengkai.ong@gmail.com>
2025-06-07 19:32:12 +0800

    feat: scaffold bidding home page

--- src/app/(school)/bidding/page.tsx
+++ src/app/(school)/bidding/page.tsx
@@ -1,7 +1,36 @@
-export default function BiddingPage() {
+import { api } from "@/common/tools/trpc/server";
+import { SearchCmdk } from "@/modules/search/components/SearchCmdk";
+import { UniversityAbbreviation } from "@prisma/client";
+
+export default async function BiddingPage() {
+  const result = await api.courses.getAllByUniAbbrv({
+    universityAbbrv: UniversityAbbreviation.SMU,
+  });
+  console.log(result);
+
   return (
-    <div className="flex justify-center">
-      Watch out for the red dot in the sidebar once this is done!
-    </div>
+    <>
+      <div className="text-xl font-bold md:text-3xl">
+        Get your personalized Bidding Recommendations here
+      </div>
+
+      <div className="bg-card">
+        <SearchCmdk />
+        <div className="text-xl font-bold md:text-3xl">View Courses</div>
+        <div>
+          {result.map((course) => {
+            return (
+              <div key={course.id}>
+                {course.code} {course.name}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <div></div>
+
+      <div>this will be the footder</div>
+    </>
   );
 }
